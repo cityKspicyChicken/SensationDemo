fc8fd005a5925fdce3890f5e64a0bf43d8ece094
Sebastian Ludwig <sebastian@lurado.de>
2014-12-13 01:59:10 +0100

    Moving gun muzzle flash with aiming direction.

--- Assets/Explosions/Scripts/MuzzleFlashAnimate.js
+++ Assets/Explosions/Scripts/MuzzleFlashAnimate.js
@@ -1,7 +1,11 @@
 
 #pragma strict
 
+public var aimingDirection : AimingDirection;
+
 function Update () {
 	transform.localScale = Vector3.one * Random.Range(0.5,1.5);
+
+    transform.rotation = Quaternion.FromToRotation(transform.parent.forward, aimingDirection.ray.direction);
 	transform.localEulerAngles.z = Random.Range(0,90.0);
 }
