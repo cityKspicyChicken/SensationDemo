c4060ee67dcc709ed782069db052c2b1955e6818
Sebastian Ludwig <sebastian@lurado.de>
2014-12-14 01:01:12 +0100

    Fixed muzzle flash animation to be properly oriented even after the character turned.

--- Assets/Explosions/Scripts/MuzzleFlashAnimate.js
+++ Assets/Explosions/Scripts/MuzzleFlashAnimate.js
@@ -6,6 +6,6 @@ public var aimingDirection : AimingDirection;
 function Update () {
 	transform.localScale = Vector3.one * Random.Range(0.5,1.5);
 
-    transform.rotation = Quaternion.FromToRotation(transform.parent.forward, aimingDirection.ray.direction);
+    transform.rotation = Quaternion.FromToRotation(Vector3.forward, aimingDirection.ray.direction);
 	transform.localEulerAngles.z = Random.Range(0,90.0);
 }
